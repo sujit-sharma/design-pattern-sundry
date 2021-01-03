17f2a1191be5d1a72c09aab3153867cbcf9a0f62
sujit <sujit.sharma@clusus.com>
2021-01-03 11:20:33 +0545

    esetting database

--- corona-info/src/main/java/com/sujit/database/DatabaseConnection.java
+++ corona-info/src/main/java/com/sujit/database/DatabaseConnection.java
@@ -20,10 +20,7 @@ public class DatabaseConnection {
                 Connection con = DriverManager.getConnection("jdbc:h2:~/test", "test", "" );
                 Statement stmt = con.createStatement();
                 //stmt.executeUpdate( "DROP TABLE table1" );
-                stmt.executeUpdate( "CREATE TABLE table1 ( user varchar(50) )" );
-                stmt.executeUpdate( "INSERT INTO table1 ( user ) VALUES ( 'Claudio' )" );
-                stmt.executeUpdate( "INSERT INTO table1 ( user ) VALUES ( 'Bernasconi' )" );
-                ResultSet rs = stmt.executeQuery("SELECT * FROM table1");
+
                 System.out.println("Generate output ");
                 while( rs.next() )
                 {
